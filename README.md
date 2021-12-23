bbe2c0cfaa045f86360bf6067d332e7d1ec7e7c5
tthvar <tthvar@berkeley.edu>
2021-12-23 02:49:48 -0800

    added email lookup/verification

--- README.md
+++ README.md
@@ -10,23 +10,38 @@ pip install -r requirements.txt
 
 ## Usage
 ```
-python TestScraper.py CONFIG_PATH LEAD_LIST_LINK PROFILE_LIST_PATH EXCEL_PATH PARSE_START
+python TestScraper.py CONFIG_PATH LEAD_LIST_LINK PROFILE_LIST_PATH EXCEL_PATH PARSE_START OUTPUT_PROFILE_DATA_PATH ROCKET_API_KEY VERIFY_API_KEY
 ```
 CONFIG_PATH = Where the configuration file is stored. This should be a text file with the first line containing username and second line containing password (ex: config.txt)
 
-LEAD_LIST_LINK = Link to lead list within sales navigator. Make sure it is in double quotes. (ex: "www.linkedin.com/xxx")
+LEAD_LIST_LINK = Link to lead list within sales navigator. Make sure it is in double quotes (ex: "www.linkedin.com/xxx"). Set to "None" if you already have crawled the lead list/have a profile_list.txt file. 
 
-PROFILE_LIST_PATH  = Where the text file where profile urls is stored. This should be an empty text file (ex: output/profile_list.txt, where you have made a folder within the script's folder titled output)
+PROFILE_LIST_PATH  = Where the text file with profile urls is stored. This should be an empty text file (ex: output/profile_list.txt, where you have made a folder within the script's folder titled output)
 
 EXCEL_PATH = Where the excel file is stored where information will be posted to (ex: output/profiles.xlsx)
 
-PARSE_START (Optional, default is 0) = Where you want to start parsing the profile text file from. When parsing a large list (> 500 profiles) you will need to use this parameter and set it to the index of the profile you want to continue reading from (i.e. line 500 -> 499, python is 0-indexed). Regardless, the script will only allow you to parse 500 profiles (more on this in the notes section).
+PARSE_START = Where you want to start parsing the profile text file from. When parsing a large list (> 250 profiles) you will need to use this parameter and set it to the index of the profile you want to continue reading from (i.e. line 500 -> 499, python is 0-indexed). Regardless, the script will only allow you to parse 250 profiles in a single run(more on this in the notes section). Set this to -1 if you have already scraped profile-level information/have a profile_data.txt file. 
+
+OUTPUT_PROFILE_DATA_PATH = Where to dump profile data as it is scraped (this should be a text file). This is to ensure that no information is lost if the script crashes due to bot detection. 
+
+ROCKET_API_KEY/VERIFY_API_KEY = API keys for Rocket Reach and Verify-Email
 
 ### Sample Usage
-
+1. End-To-End
 ```
-python TestScraper.py config.txt "https://www.linkedin.com/sales/search/people?savedSearchId=REDACTED" output/profile_list.txt output/profiles.xlsx
+python TestScraper.py config.txt "https://www.linkedin.com/sales/search/REDACTED" output/profile_list.txt output/profiles.xlsx 0 output/profiles_data.txt ROCKET_API_KEY VERIFY_API_KEY
+```
+
+2. Profile Parsing & Email Lookup/Verification
+```
+python TestScraper.py config.txt "None" output/profile_list.txt output/profiles.xlsx 10 output/profiles_data.txt ROCKET_API_KEY VERIFY_API_KEY
+```
+
+
+3. Email Lookup/Verification
+```
+python TestScraper.py config.txt "None" output/profile_list.txt output/profiles.xlsx -1 output/profiles_data.txt ROCKET_API_KEY VERIFY_API_KEY
 ```
 
 ## To Note
-1. The script will automatically stop running after 500 profiles are viewed. This is to work around Sales Navigator's viewing limits (if exceeded, can lead to an account ban, important to not sidestep). Continue running the script from where you left off using the text file containing profile urls. 
+1. The script will automatically stop running after 250 profiles are viewed. This is to work around Sales Navigator's viewing limits (if exceeded, can lead to an account ban, important to not sidestep). Continue running the script from where you left off using the text file containing profile urls. 
--- TestScraper.py
+++ TestScraper.py
@@ -11,7 +11,19 @@ import time
 import re
 import pandas as pd
 
-#Usage: python TestScraper.py config.txt "https://www.linkedin.com/sales/search/people?REDACTED" output/profile_list.txt output/profiles.xlsx 0
+import json
+import requests
+import nltk
+
+#Usage - End to End
+# python TestScraper.py config.txt "https://www.linkedin.com/sales/search/REDACTED" output/profile_list.txt output/profiles.xlsx 0 output/profiles_data.txt ROCKET_API_KEY VERIFY_API_KEY
+
+#Usage - Just Profile Parsing
+# python TestScraper.py config.txt "None" output/profile_list.txt output/profiles.xlsx 51 output/profiles_data.txt ROCKET_API_KEY VERIFY_API_KEY
+
+#Usage - Just Email Lookup/Verification
+# python TestScraper.py config.txt "None" output/profile_list.txt output/profiles.xlsx -1 output/profiles_data.txt ROCKET_API_KEY VERIFY_API_KEY
+
 
 def main():
 	args = sys.argv[1:]
@@ -20,26 +32,51 @@ def main():
 	LIST_LINK = args[1]
 	PROFILE_PATH = args[2]
 	XLX_OUTPUT_PATH = args[3]
-	parse_only, parse_start = False, 0
-	if len(args) == 5:
-		parse_only = True
-		parse_start = int(args[4])
+	parse_start = int(args[4])
+	PROFILE_DATA_PATH = args[5]
+	ROCKET_KEY = args[6]
+	VERIFY_KEY = args[7]
+
+	retrieve_profiles = (LIST_LINK != "NONE")
 
 	start_time = time.time()
 	
-	driver = login(CONFIG_PATH)
+	driver = None
 	
-	if not parse_only:
+	profile_urls = None
+	if not retrieve_profiles:
+		driver = login(CONFIG_PATH)
 		profile_urls = get_profile_urls(driver, LIST_LINK, PROFILE_PATH)
-	
-	profile_urls = read_profile_urls(PROFILE_PATH)
+	else:
+		print("NO URL RETRIEVAL!")
+		profile_urls = read_profile_urls(PROFILE_PATH)
 
-	parse_profiles(driver, profile_urls, XLX_OUTPUT_PATH, parse_start)
+	profiles_data = None
+	if parse_start != -1:
+		profiles_data = None
+		if parse_start > 0:
+			with open(PROFILE_DATA_PATH) as datafile:
+				profiles_data = json.loads(datafile.read())
+		if driver == None:
+			driver = login(CONFIG_PATH)
+		profiles_data = parse_profiles(driver, profile_urls, XLX_OUTPUT_PATH, parse_start, profiles_data, PROFILE_DATA_PATH)
+	else:
+		print("NO PARSES!")
+		print(PROFILE_DATA_PATH)
+		with open(PROFILE_DATA_PATH) as datafile:
+			profiles_data = json.loads(datafile.read())
+
+	if driver:
+		driver.close()
+
+	profiles_data_email = verify_emails(profiles_data, ROCKET_KEY, VERIFY_KEY, PROFILE_DATA_PATH)
+
+	load_excel(profiles_data_email, XLX_OUTPUT_PATH)
 
 	execution_time = (time.time() - start_time)
 	print('Execution time in seconds: ' + str(execution_time))
 	
-	driver.close()
+	
 
 
 def read_config(CONFIG_PATH):
@@ -69,10 +106,7 @@ def login(CONFIG_PATH):
 	driver = webdriver.Chrome(options=chrome_options,executable_path="./chromedriver")
 	driver.get("https://www.linkedin.com/sales/login")
 	time.sleep(1)
-	#print(driver.get("https://www.linkedin.com"))
-	print("REACHED 1")
 	WebDriverWait(driver, 5).until(EC.frame_to_be_available_and_switch_to_it((By.CSS_SELECTOR,".authentication-iframe")))
-	print("REACHED 2")
 	driver.find_element_by_css_selector('#username').send_keys(user)
 	driver.find_element_by_css_selector('#password').send_keys(pw)
 	print("LOGGED IN")
@@ -81,7 +115,6 @@ def login(CONFIG_PATH):
 
 def get_profile_urls(driver, list_link, OUTPUT_PROFILE_PATH):
 	driver.get(list_link);
-	#Iterate over pages
 	idx = 2
 	profile_urls = []
 	buttons = []
@@ -107,9 +140,6 @@ def get_profile_urls(driver, list_link, OUTPUT_PROFILE_PATH):
 		except:
 			break
 
-		# if idx > 2:
-		# 	break
-
 		button.click()
 		time.sleep(3)
 		print("# Profiles", len(profile_urls))
@@ -124,40 +154,39 @@ def get_profile_urls(driver, list_link, OUTPUT_PROFILE_PATH):
 
 	return profile_urls
 
-def parse_profiles(driver, profile_urls, XLX_OUTPUT_PATH, parse_start):
+def parse_profiles(driver, profile_urls, XLX_OUTPUT_PATH, parse_start, profile_data, PROFILE_DATA_PATH):
 	profiles_data = {}
-	profiles_data["First Name"] = []
-	profiles_data["Last Name"] = []
-	profiles_data["# Years in Current Role"] = []
-	profiles_data["Linkedin URL"] = []
-	profiles_data["Email"] = []
-	profiles_data["Email Verified? Yes/No"] = []
-	profiles_data["Undergraduate College / University"] = []
-	profiles_data["Location (city) of individual"] = []
-	profiles_data["Location (state) of individual"] = []
-	profiles_data["Current Company"] = []
-	profiles_data["Current Company URL"] = []
-	profiles_data["# LinkedIn Employees at Current Company"] = []
-	profiles_data["Company industry category"] = []
-	profiles_data["Location (city) of Company"] = []
-	profiles_data["Location (state) of Company"] = []
+	if profile_data != None:
+		profiles_data = profile_data
+	else:
+		profiles_data["Name"] = []
+		profiles_data["# Years in Current Role"] = []
+		profiles_data["Linkedin URL"] = []
+		profiles_data["Email"] = []
+		profiles_data["Email Verified? Yes/No"] = []
+		profiles_data["Undergraduate College / University"] = []
+		profiles_data["Location (city) of individual"] = []
+		profiles_data["Location (state) of individual"] = []
+		profiles_data["Current Company"] = []
+		profiles_data["Current Company URL"] = []
+		profiles_data["# LinkedIn Employees at Current Company"] = []
+		profiles_data["Company industry category"] = []
+		profiles_data["Location (city) of Company"] = []
+		profiles_data["Location (state) of Company"] = []
 	
 
 	for idx, prof_url in enumerate(profile_urls):
-		if idx != parse_start:
+		print("INDEX", idx)
+		if idx < parse_start:
 			continue
-		if idx >= parse_start + 500:
+		if idx >= (parse_start + 250):
 			break
 		print("PROFILE {}".format(idx))
 		driver.get(prof_url)
 		name_div = driver.find_element_by_xpath('.//span[@class = "profile-topcard-person-entity__name t-24 t-black t-bold"]')
-		name_words = name_div.text
-		name_words = name_words.split(" ")
-		fname = re.sub("[^a-zA-Z]+", "", name_words[0])
-		lname = re.sub("[^a-zA-Z]+", "", name_words[1])
-		print("Name", fname + " " + lname)
-		#profiles_data["First Name"].append(fname)
-		#profiles_data["Last Name"].append(lname)
+		name = name_div.text
+		print("Name", name)
+
 		time.sleep(1)
 		title_div = driver.find_element_by_xpath('.//span[@class = "profile-topcard__summary-position-title"]')
 		title = title_div.text
@@ -166,7 +195,6 @@ def parse_profiles(driver, profile_urls, XLX_OUTPUT_PATH, parse_start):
 		time_div = driver.find_element_by_xpath('.//span[@class = "profile-topcard__time-period-bullet"]')
 		num_years = time_div.text
 		print("# Years", num_years)
-
 
 		location_div = driver.find_element_by_xpath('.//div[@class = "profile-topcard__location-data inline t-14 t-black--light mr5"]')
 		prof_loc = location_div.text
@@ -179,7 +207,7 @@ def parse_profiles(driver, profile_urls, XLX_OUTPUT_PATH, parse_start):
 		print("Profile Location", prof_city + "," + prof_state)
 
 		#Get Education Info
-
+		time.sleep(1)
 		edu_div = driver.find_elements_by_xpath('.//li[@class = "profile-education display-flex align-items-flex-start"]')
 		undergrad_text = ""
 		for item in edu_div:
@@ -198,7 +226,6 @@ def parse_profiles(driver, profile_urls, XLX_OUTPUT_PATH, parse_start):
 		print("Undergrad", undergrad_text)
 
 		#Get Company-Level Info
-		time.sleep(1)
 		company_div = None
 		company_link, company_text = "", ""
 		
@@ -210,19 +237,17 @@ def parse_profiles(driver, profile_urls, XLX_OUTPUT_PATH, parse_start):
 			company_div = driver.find_element_by_xpath('.//span[@class = "t-14 t-black t-bold"]')
 			company_text = company_div.text
 
-
 		industry, num_employees, comp_city, comp_state = "","","",""
 		if company_link != "":
 			driver.get(company_link)
-			time.sleep(1)
+			time.sleep(1.5)
 			industry_div = driver.find_element_by_xpath('.//div[@class = "t-14"]')
 			industry_raw = industry_div.text
 			industry = industry_raw.split("·")[0]
 
-			time.sleep(1)
 			num_employees_div = driver.find_element_by_xpath('.//a[@class = "ember-view link-without-visited-and-hover-state"]')
 			is_thousand = "K" in num_employees_div.text
-			#print("RAW EMPLOYEES", num_employees_div.text, str(is_thousand))
+
 			num_employees = float(re.sub("[^\d.]+", "", num_employees_div.text))
 			if is_thousand: num_employees *= 1000
 			num_employees = int(num_employees)
@@ -238,8 +263,7 @@ def parse_profiles(driver, profile_urls, XLX_OUTPUT_PATH, parse_start):
 		print("Company Info", company_link + " " + company_text + " "
 			+ industry + " " + str(num_employees) + " " + comp_city + ", " + comp_state)
 
-		profiles_data["First Name"].append(fname)
-		profiles_data["Last Name"].append(lname)
+		profiles_data["Name"].append(name)
 		profiles_data["# Years in Current Role"].append(num_years)
 		profiles_data["Linkedin URL"].append(prof_url)
 		profiles_data["Email"].append("")
@@ -263,9 +287,53 @@ def parse_profiles(driver, profile_urls, XLX_OUTPUT_PATH, parse_start):
 			profiles_data["Location (city) of Company"].append(comp_loc)
 			profiles_data["Location (state) of Company"].append(comp_loc)
 		
-		profiles_df = pd.DataFrame.from_dict(profiles_data)
-		profiles_df.to_excel(XLX_OUTPUT_PATH, index = False)
 
+		## Save content to avoid running into viewing limit errors
+		with open(PROFILE_DATA_PATH, 'w') as convert_file:
+			convert_file.write(json.dumps(profiles_data))
+
+	return profiles_data
+
+
+def verify_emails(profiles_data, ROCKET_KEY, VERIFY_KEY, PROFILE_DATA_PATH):
+	names = profiles_data["Name"]
+	companies = profiles_data["Current Company"]
+	#Rocket Reach - Email Lookup
+	rocket_url = "https://api.rocketreach.co/v2/api/lookupProfile"
+	rocket_headers = {"Api-Key": ROCKET_KEY}
+
+	emails, verifies = [], []
+	for idx, (name, company) in enumerate(zip(names, companies)):
+		#Lookup Email
+		rock_r = requests.get(rocket_url, data = {"name":name, "current_employer":company}, headers = rocket_headers)
+		data = rock_r.json()
+		email = ""
+		if 'detail' not in data:
+			email = data["current_work_email"]
+
+		verified = "No"
+		if email != "":
+			verify_url = "https://app.verify-email.org/api/v1/{0}/verify/{1}".format(VERIFY_KEY, email)
+			r = requests.get(verify_url)
+			data = r.json()
+			if data['status'] == 1:
+				verified = "Yes"
+		
+		print(idx, name, company, email, verified)
+		emails.append(email)
+		verifies.append(verified)
+
+	profiles_data["Email"] = emails
+	profiles_data["Email Verified? Yes/No"] = verifies
+
+	with open(PROFILE_DATA_PATH, 'w') as convert_file:
+		convert_file.write(json.dumps(profiles_data))
+
+	return profiles_data
+
+def load_excel(profiles_data, XLX_OUTPUT_PATH):
+	profiles_df = pd.DataFrame.from_dict(profiles_data)
+	profiles_df.to_excel(XLX_OUTPUT_PATH, index = False)
 
 if __name__ == "__main__":
     main()
